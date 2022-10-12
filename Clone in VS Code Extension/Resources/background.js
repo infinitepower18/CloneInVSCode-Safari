caa6be6f091460ee700f9343a00aebb7774412b9
infinitepower18 <44692189+infinitepower18@users.noreply.github.com>
2022-10-13 00:10:49 +0100

    add vscode insiders, vscodium, vscodium insiders support

--- Clone in VS Code Extension/Resources/background.js
+++ Clone in VS Code Extension/Resources/background.js
@@ -2,16 +2,27 @@ chrome.action.onClicked.addListener((tab) => {
   chrome.storage.sync.get(
     {
       protocol: "HTTPS",
+      application: "VS Code",
     },
     function (items) {
       chrome.tabs.query({ active: true, lastFocusedWindow: true }, (tabs) => {
         const url = new URL(tabs[0].url);
         const path = url.pathname.split("/");
+        if (items.application == "VS Code") {
+          openApp = "vscode";
+        } else if (items.application == "VS Code Insiders") {
+          openApp = "vscode-insiders";
+        } else if (items.application == "VSCodium") {
+          openApp = "vscodium";
+        } else if (items.application == "VSCodium Insiders") {
+          openApp = "vscodium-insiders";
+        }
         if (path[1] !== undefined && path[2] !== undefined) {
           if (items.protocol == "HTTPS") {
             chrome.tabs.create({
               url:
-                "vscode://vscode.git/clone?url=https://" +
+              openApp +
+              "://vscode.git/clone?url=https://" +
                 url.hostname +
                 "/" +
                 path[1] +
@@ -22,7 +33,8 @@ chrome.action.onClicked.addListener((tab) => {
           } else if (items.protocol == "SSH") {
             chrome.tabs.create({
               url:
-                "vscode://vscode.git/clone?url=git@" +
+              openApp +
+              "://vscode.git/clone?url=git@" +
                 url.hostname +
                 ":" +
                 path[1] +
