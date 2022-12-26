3d29213c9ed67cc3cb2785dd24cadc91a5dcc46f
Ahnaf Mahmud <44692189+infinitepower18@users.noreply.github.com>
2022-12-26 14:48:33 +0000

    Update Script.js

--- Clone in VS Code/Resources/Script.js
+++ Clone in VS Code/Resources/Script.js
@@ -1,4 +1,11 @@
-function show(enabled) {
+function show(enabled, useSettingsInsteadOfPreferences) {
+    if (useSettingsInsteadOfPreferences) {
+        document.getElementsByClassName('state-on')[0].innerText = "Clone in VS Code’s extension is currently on. You can turn it off in the Extensions section of Safari Settings.";
+        document.getElementsByClassName('state-off')[0].innerText = "Clone in VS Code’s extension is currently off. You can turn it on in the Extensions section of Safari Settings.";
+        document.getElementsByClassName('state-unknown')[0].innerText = "You can turn on Clone in VS Code’s extension in the Extensions section of Safari Settings.";
+        document.getElementsByClassName('open-preferences')[0].innerText = "Quit and Open Safari Settings…";
+    }
+
     if (typeof enabled === "boolean") {
         document.body.classList.toggle(`state-on`, enabled);
         document.body.classList.toggle(`state-off`, !enabled);
